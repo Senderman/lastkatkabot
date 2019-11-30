859f2b1bc98d94ec4532be1d405854718e5eb411
Fyodor Doletov <doletov.fyodor@yandex.ru>
2019-11-30 04:03:06 +0300

    anime2kotlin

---
+++ src/main/java/com/senderman/anitrackerbot/DBService.kt
@@ -0,0 +1,12 @@
+package com.senderman.anitrackerbot
+
+interface DBService {
+    fun saveAnime(id: Int, userId: Int, url: String)
+    fun getAnimeUrl(id: Int, userId: Int): String
+    fun deleteAnime(id: Int, userId: Int)
+    fun dropUser(userId: Int)
+    fun totalAnimes(userId: Int): Int
+    fun idExists(id: Int, userId: Int): Boolean
+    fun urlExists(url: String, userId: Int): Boolean
+    fun getAllAnimes(userId: Int): Map<Int, String>
+}
