a60b6c9bcf9ab7208846e87de542686b1293cb6a
Gbball <48598980+Vezono2@users.noreply.github.com>
2019-12-01 19:24:47 +0200

    Update UsercommandsHandler.java

--- src/main/java/com/senderman/lastkatkabot/handlers/UsercommandsHandler.java
+++ src/main/java/com/senderman/lastkatkabot/handlers/UsercommandsHandler.java
@@ -126,7 +126,7 @@ public class UsercommandsHandler {
 
     public void marryme(Message message) {
         boolean marryById = message.getText().length() - message.getText().replace(" ", "").length() == 1;
-        if (message.getFrom().getId().equals(message.getReplyToMessage().getFrom().getId()) || message.getReplyToMessage().getFrom().getBot() || marryById || message.isUserMessage())
+        if (message.getFrom().getId().equals(message.getReplyToMessage().getFrom().getId()) || message.getReplyToMessage().getFrom().getBot() || message.isUserMessage())
             return;
         
         var chatId = message.getChatId();
