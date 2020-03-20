064813842cbd139fbdb82e43918725fffa6056ef
Gbball <48598980+Vezono2@users.noreply.github.com>
2020-03-20 16:33:47 +0200

    Create AdoptChild.kt

---
+++ src/main/kotlin/com/senderman/lastkatkabot/usercommands/AdoptChild.kt
@@ -0,0 +1,10 @@
+package com.senderman.lastkatkabot.usercommands
+
+import com.annimon.tgbotsmodule.api.methods.Methods
+import com.senderman.lastkatkabot.Callbacks
+import com.senderman.lastkatkabot.LastkatkaBotHandler
+import com.senderman.neblib.CommandExecutor
+import com.senderman.neblib.TgUser
+import org.telegram.telegrambots.meta.api.objects.Message
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton
