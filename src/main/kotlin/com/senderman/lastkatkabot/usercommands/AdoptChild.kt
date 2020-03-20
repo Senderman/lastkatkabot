43b8a1645c50e875ce0d120b6339a53e821a419d
Gbball <48598980+Vezono2@users.noreply.github.com>
2020-03-20 16:42:57 +0200

    Update AdoptChild.kt

--- src/main/kotlin/com/senderman/lastkatkabot/usercommands/AdoptChild.kt
+++ src/main/kotlin/com/senderman/lastkatkabot/usercommands/AdoptChild.kt
@@ -8,3 +8,37 @@ import com.senderman.neblib.TgUser
 import org.telegram.telegrambots.meta.api.objects.Message
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton
+
+class AdoptChild(private val handler: LastkatkaBotHandler) : CommandExecutor {
+    override val command: String
+        get() = "/adoptchild"
+    override val desc: String
+        get() = "(reply) усыновить ребенка. Требуется согласие обоих супругов."
+
+    override fun execute(message: Message) {
+        if (!message.isReply || message.isUserMessage) return
+        val markup = InlineKeyboardMarkup()
+        markup.keyboard = listOf(listOf(
+            InlineKeyboardButton().apply {
+                text = "Принять"
+                callbackData = Callbacks.CALLBACK_CAKE_OK + message.text
+                    .replace("/cake", "")
+            },
+            InlineKeyboardButton().apply {
+                text = "Отказаться"
+                callbackData = Callbacks.CALLBACK_CAKE_NOT + message.text
+                    .replace("/cake", "")
+            }
+        ))
+        Methods.deleteMessage(message.chatId, message.messageId).call(handler)
+        val adult = TgUser(message.from)
+        val child = TgUser(message.replyToMessage.from)
+        handler.sendMessage(
+            Methods.sendMessage()
+                .setChatId(message.chatId)
+                .setText(
+                    "\uD83C\uDF82 ${luckyOne.name} пользователь ${presenter.name} подарил вам тортик " +
+                            message.text.replace("/cake", "")
+                )
+                .setReplyToMessageId(message.replyToMessage.messageId)
+                .setReplyMarkup(markup)
