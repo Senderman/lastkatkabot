9a8b79cc00e10535f1c8ea4fd6250596d593413a
Брит Серпинский <48598980+Vezono@users.noreply.github.com>
2022-02-18 18:54:37 +0200

    Update WeatherCommand.java

--- src/main/java/com/senderman/lastkatkabot/command/user/WeatherCommand.java
+++ src/main/java/com/senderman/lastkatkabot/command/user/WeatherCommand.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
-@Component
+//@Component
 public class WeatherCommand implements CommandExecutor {
 
     private final UserStatsService userStats;
