2641dcb725d895e01871ff6b34f71c1d46efb0b4
Fyodor Doletov <doletov.fyodor@yandex.ru>
2022-03-16 18:06:00 +0300

    [Weather] Move to separate thread pool

--- src/main/java/com/senderman/lastkatkabot/command/user/WeatherCommand.java
+++ src/main/java/com/senderman/lastkatkabot/command/user/WeatherCommand.java
@@ -12,16 +12,19 @@ import org.jetbrains.annotations.Nullable;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.concurrent.ExecutorService;
 
 @Component
 public class WeatherCommand implements CommandExecutor {
 
     private final UserStatsService userStats;
     private final WeatherService weatherService;
+    private final ExecutorService threadPool;
 
-    public WeatherCommand(UserStatsService userStats, WeatherService weatherService) {
+    public WeatherCommand(UserStatsService userStats, WeatherService weatherService, ExecutorService threadPool) {
         this.userStats = userStats;
         this.weatherService = weatherService;
+        this.threadPool = threadPool;
     }
 
     @Override
@@ -36,24 +39,26 @@ public class WeatherCommand implements CommandExecutor {
 
     @Override
     public void accept(@NotNull MessageContext ctx) {
-        ctx.setArgumentsLimit(1);
-        try {
-            String cityLink = getCityLinkFromMessageData(ctx);
-            var text = forecastToString(weatherService.getWeatherByCityLink(cityLink));
-            ctx.replyToMessage(text).callAsync(ctx.sender);
-            // save last defined city in db (we won't get here if exception is occurred)
-            saveCityLinkToDb(cityLink, ctx.user().getId());
-        } catch (NoSuchCityException e) {
-            ctx.replyToMessage("Город не найден").callAsync(ctx.sender);
-        } catch (NoCitySpecifiedException e) {
-            ctx.replyToMessage("Вы не указали город! (/weather город). Бот запомнит ваш выбор.").callAsync(ctx.sender);
-        } catch (ParseException e) {
-            ctx.replyToMessage("Ошибка обработки запроса: " + e.getMessage()).callAsync(ctx.sender);
-            throw new RuntimeException(e);
-        } catch (IOException e) {
-            ctx.replyToMessage("Ошибка соединения с сервисом погоды").callAsync(ctx.sender);
-            throw new RuntimeException(e);
-        }
+        threadPool.execute(() -> {
+            ctx.setArgumentsLimit(1);
+            try {
+                String cityLink = getCityLinkFromMessageData(ctx);
+                var text = forecastToString(weatherService.getWeatherByCityLink(cityLink));
+                ctx.replyToMessage(text).callAsync(ctx.sender);
+                // save last defined city in db (we won't get here if exception is occurred)
+                saveCityLinkToDb(cityLink, ctx.user().getId());
+            } catch (NoSuchCityException e) {
+                ctx.replyToMessage("Город не найден").callAsync(ctx.sender);
+            } catch (NoCitySpecifiedException e) {
+                ctx.replyToMessage("Вы не указали город! (/weather город). Бот запомнит ваш выбор.").callAsync(ctx.sender);
+            } catch (ParseException e) {
+                ctx.replyToMessage("Ошибка обработки запроса: " + e.getMessage()).callAsync(ctx.sender);
+                throw new RuntimeException(e);
+            } catch (IOException e) {
+                ctx.replyToMessage("Ошибка соединения с сервисом погоды").callAsync(ctx.sender);
+                throw new RuntimeException(e);
+            }
+        });
     }
 
     String getCityLinkFromMessageData(MessageContext ctx) throws NoSuchCityException, IOException, NoCitySpecifiedException {
