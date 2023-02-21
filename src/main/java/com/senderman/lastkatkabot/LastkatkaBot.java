6d655a86da7e0e048655bd293217afa70df5e213
Fyodor Doletov <doletov.fyodor@yandex.ru>
2023-02-21 18:15:46 +0300

    [metrics] add /metrics and /prometheus metrics

--- src/main/java/com/senderman/lastkatkabot/LastkatkaBot.java
+++ src/main/java/com/senderman/lastkatkabot/LastkatkaBot.java
@@ -3,25 +3,19 @@ package com.senderman.lastkatkabot;
 import com.annimon.tgbotsmodule.BotModule;
 import com.annimon.tgbotsmodule.Runner;
 import com.annimon.tgbotsmodule.beans.Config;
-import io.micronaut.context.ApplicationContext;
-import io.micronaut.runtime.ApplicationConfiguration;
-import io.micronaut.runtime.EmbeddedApplication;
 import io.micronaut.runtime.Micronaut;
+import io.micronaut.scheduling.annotation.Scheduled;
 import jakarta.inject.Singleton;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 
 @Singleton
-public class LastkatkaBot implements BotModule, EmbeddedApplication<LastkatkaBot> {
+public class LastkatkaBot implements BotModule {
 
-    private final ApplicationContext context;
-    private final ApplicationConfiguration configuration;
     private final BotHandler botHandler;
 
-    public LastkatkaBot(ApplicationContext context, ApplicationConfiguration configuration, BotHandler botHandler) {
-        this.context = context;
-        this.configuration = configuration;
+    public LastkatkaBot(BotHandler botHandler) {
         this.botHandler = botHandler;
     }
 
@@ -29,35 +23,14 @@ public class LastkatkaBot implements BotModule, EmbeddedApplication<LastkatkaBot
         Micronaut.run(LastkatkaBot.class, args);
     }
 
-    @Override
-    public @NotNull LastkatkaBot start() {
-        Runner.run(List.of(this));
-        return this;
+    @Scheduled(initialDelay = "1s")
+    public void runBot() {
+        new Thread(() -> Runner.run(List.of(this))).start();
     }
 
     @Override
     public @NotNull BotHandler botHandler(@NotNull Config config) {
         return botHandler;
-    }
-
-    @Override
-    public ApplicationContext getApplicationContext() {
-        return context;
-    }
-
-    @Override
-    public ApplicationConfiguration getApplicationConfiguration() {
-        return configuration;
-    }
-
-    @Override
-    public boolean isRunning() {
-        return true;
-    }
-
-    @Override
-    public boolean isServer() {
-        return true;
     }
 
 }
