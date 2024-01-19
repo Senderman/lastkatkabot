ce6b71ab9776b812e4c6b9ca2357095914270872
Fyodor Doletov <doletov.fyodor@yandex.ru>
2024-01-19 12:39:56 +0300

    [util] simplify user name extraction

--- src/main/java/com/senderman/lastkatkabot/util/TelegramUsersHelper.java
+++ src/main/java/com/senderman/lastkatkabot/util/TelegramUsersHelper.java
@@ -1,17 +1,13 @@
 package com.senderman.lastkatkabot.util;
 
-import com.annimon.tgbotsmodule.api.methods.Methods;
-import com.annimon.tgbotsmodule.services.CommonAbsSender;
 import com.senderman.lastkatkabot.config.BotConfig;
 import com.senderman.lastkatkabot.feature.l10n.context.L10nMessageContext;
 import com.senderman.lastkatkabot.feature.userstats.service.UserStatsService;
 import jakarta.inject.Singleton;
 import org.jetbrains.annotations.NotNull;
 import org.telegram.telegrambots.meta.api.objects.User;
-import org.telegram.telegrambots.meta.api.objects.chatmember.ChatMember;
 
 import java.util.Objects;
-import java.util.Optional;
 import java.util.Set;
 
 @Singleton
@@ -51,36 +47,17 @@ public class TelegramUsersHelper {
 
     /**
      * Get the user's name within {@link User} object by userId
-     * It tries to find user's name in CHAT_USERS table. If it fails, it asks it from telegram. And if this fails,
-     * the returned {@link User} will have default firstName.
-     * Note that if known user's firstName is blank (contains only whitespace characters), it's name will also be set
-     * to default.
+     * It tries to find user's name in CHAT_USERS table. If it's empty or blank, it will have the default name
      *
      * @param userId id of the user
      * @param ctx    localized message context
      * @return {@link User} object with user's firstName
      */
     public User findUserFirstName(long userId, L10nMessageContext ctx) {
-        return Optional.of(userStatsService.findById(userId))
-                .map(u -> new User(u.getUserId(), Objects.requireNonNull(u.getName(), ""), false))
-                .or(() -> getUserDataFromTelegram(userId, ctx.sender))
-                .map(u -> {
-                    if (u.getFirstName().isBlank())
-                        u.setFirstName(ctx.getString("common.noName"));
-                    return u;
-                })
-                .orElseGet(() -> new User(userId, ctx.getString("common.noName"), false));
-    }
-
-    /**
-     * Get data about user from telegram by userId
-     *
-     * @param userId id of the user
-     * @param sender telegram handler
-     * @return {@link Optional<User>} object with user's data
-     */
-    public Optional<User> getUserDataFromTelegram(long userId, CommonAbsSender sender) {
-        var member = Methods.getChatMember(userId, userId).call(sender);
-        return Optional.ofNullable(member).map(ChatMember::getUser);
+        var user = userStatsService.findById(userId);
+        if (user.getName() == null || user.getName().isBlank()) {
+            user.setName(ctx.getString("common.noName"));
+        }
+        return new User(user.getUserId(), user.getName(), false);
     }
 }
