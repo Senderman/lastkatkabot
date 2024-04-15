993bcf3e1d1d045163e764b7f0f3317fd9117ab6
Fyodor Doletov <doletov.fyodor@yandex.ru>
2024-04-15 21:55:59 +0300

    [http] fix check

--- src/main/java/com/senderman/lastkatkabot/feature/http/command/HttpCommand.java
+++ src/main/java/com/senderman/lastkatkabot/feature/http/command/HttpCommand.java
@@ -26,18 +26,18 @@ public class HttpCommand implements CommandExecutor {
             return;
         }
 
-        try {
-            String arg = ctx.argument(0);
-            Integer.parseInt(arg);
-            String message = "%s<a href=\"https://http.cat/images/%s.jpg\">\u200B</a>".formatted(arg, arg);
-            var m = ctx.replyToMessage(message).call(ctx.sender);
-            // since there's a method preprocessor that disables webPagePreview on SendMessage method,
-            // we use EditMessage to re-enable it
-            Methods.editMessageText(m.getChatId(), m.getMessageId(), message)
-                    .enableWebPagePreview()
-                    .callAsync(ctx.sender);
-        } catch (NumberFormatException e) {
+        String arg = ctx.argument(0);
+        if (!arg.matches("\\d{3}")) {
             ctx.replyToMessage(ctx.getString("http.isNumber")).callAsync(ctx.sender);
+            return;
         }
+
+        String message = "%s<a href=\"https://http.cat/images/%s.jpg\">\u200B</a>".formatted(arg, arg);
+        var m = ctx.replyToMessage(message).call(ctx.sender);
+        // since there's a method preprocessor that disables webPagePreview on SendMessage method,
+        // we use EditMessage to re-enable it
+        Methods.editMessageText(m.getChatId(), m.getMessageId(), message)
+                .enableWebPagePreview()
+                .callAsync(ctx.sender);
     }
 }
