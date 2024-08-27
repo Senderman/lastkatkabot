64cf18e4a264ddf8b3f1d71d6320842e81a1486d
Fyodor Doletov <doletov.fyodor@yandex.ru>
2024-08-27 07:53:04 +0300

    fix(eng): flag

--- src/main/java/com/senderman/lastkatkabot/feature/l10n/Locale.java
+++ src/main/java/com/senderman/lastkatkabot/feature/l10n/Locale.java
@@ -1,7 +1,7 @@
 package com.senderman.lastkatkabot.feature.l10n;
 
 public enum Locale {
-    ENGLISH("\uD83C\uDDFB\uD83C\uDDEC English", "en"),
+    ENGLISH("\uD83C\uDDEC\uD83C\uDDE7 English", "en"),
     RUSSIAN("\uD83C\uDDF7\uD83C\uDDFA Русский", "ru"),
     UKRAINIAN("\uD83C\uDDFA\uD83C\uDDE6 Украïнська", "uk");
 
