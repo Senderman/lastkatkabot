6c18fab270824045033d89b1f035bb0d7f4a7931
Fyodor Doletov <doletov.fyodor@yandex.ru>
2024-08-27 07:41:08 +0300

    feat(eng): final preparations

--- src/main/java/com/senderman/lastkatkabot/feature/l10n/Locale.java
+++ src/main/java/com/senderman/lastkatkabot/feature/l10n/Locale.java
@@ -1,6 +1,7 @@
 package com.senderman.lastkatkabot.feature.l10n;
 
 public enum Locale {
+    ENGLISH("\uD83C\uDDFB\uD83C\uDDEC English", "en"),
     RUSSIAN("\uD83C\uDDF7\uD83C\uDDFA Русский", "ru"),
     UKRAINIAN("\uD83C\uDDFA\uD83C\uDDE6 Украïнська", "uk");
 
