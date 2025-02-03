69232b5ef87d535f4417fcc7a4cf20aa3f15d5ff
Fyodor Doletov <doletov.fyodor@yandex.ru>
2025-02-04 00:18:55 +0300

    feature(weather): add retries

--- src/main/java/com/senderman/lastkatkabot/feature/weather/service/WttrClient.java
+++ src/main/java/com/senderman/lastkatkabot/feature/weather/service/WttrClient.java
@@ -6,6 +6,7 @@ import io.micronaut.http.annotation.Header;
 import io.micronaut.http.annotation.PathVariable;
 import io.micronaut.http.annotation.QueryValue;
 import io.micronaut.http.client.annotation.Client;
+import io.micronaut.retry.annotation.Retryable;
 
 import java.util.Optional;
 
@@ -15,6 +16,7 @@ public interface WttrClient {
     String SHORT_FORMAT = "%l\n%t\n%f\n%c%C\n%w\n%h\n%P\n%m";
 
     @Get("{location}?m0AFTMq&lang={lang}")
+    @Retryable
     Optional<String> getShortWeather(
             @PathVariable("location") String location,
             @Nullable @QueryValue(value = "format", defaultValue = SHORT_FORMAT) String format,
@@ -23,6 +25,7 @@ public interface WttrClient {
 
     @Get("{location}?TdM&lang={lang}")
     @Header(name = "User-Agent", value = "curl/7.64.1 (x86_64-pc-linux-gnu) libcurl/7.64.1 OpenSSL/1.1.1b zlib/1.2.11")
+    @Retryable
     Optional<String> getFullWeatherAscii(
             @PathVariable("location") String location,
             @PathVariable("lang") String lang
